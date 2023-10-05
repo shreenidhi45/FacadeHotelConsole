dbb890d636c9ef50773a2dbddc82d93714052a57
Shreenidhi <Shreenidhi@ACER1214>
2023-10-05 23:32:08 +0530

    Add project files.

---
+++ FacadeHotelConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using FacadeHotelLibrary.Model;
+namespace FacadeHotelConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HotelReception reception = new HotelReception();
+            reception.getIndianMenu();
+            reception.getItalianMenu();
+        }
+    }
+}
